89d61a1386e639cd7a9f0a68f3d80d21fc3c03cc
aishaALKM <70426536+aishaALKM@users.noreply.github.com>
2022-03-11 17:49:12 +0300

    not done yet :)

--- ios_homework1/ios_homework1/ContentView.swift
+++ ios_homework1/ios_homework1/ContentView.swift
@@ -11,56 +11,127 @@ struct ContentView: View {
     var body: some View {
         
         
-        ZStack{
+        
         VStack{
             Text("My favorite series")
+                .font(.title)
+                .fontWeight(.black)
             
+        
+            HStack{
+             
             Image("blacklist")
                 .resizable()
+                .padding()
                 .frame(width: 130, height: 130)
                 .scaledToFit()
+              
+             
+                Text("The Black List")
+                    .font(.callout)
+                    .fontWeight(.thin)
+                    .multilineTextAlignment(.center)
+                Spacer()
+                Text("7.3")
+                Text("⭐️")
+                
+                   
+            }
+            .padding()
+            
+            
+            
+            
+            VStack{
+                Spacer()
+                HStack{
+                    Image("uri")
+                        .resizable()
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        .frame(width: 130, height: 130)
+                        .scaledToFit()
+                    
+                    Text("Anne with an E")
+                        .font(.callout)
+                        .fontWeight(.thin)
+                        .multilineTextAlignment(.center)
+                    
+                    Spacer()
+                    Text("7.8")
+                    Text("⭐️")
+                    
+                 
+                  
+                }
+                
+            }
+            
+            VStack{
+                Spacer()
+                HStack{
+                    Image("image5")
+                        .resizable()
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        .frame(width: 130, height: 130)
+                        .scaledToFit()
+                    Text("Stranger things")
+                        .font(.callout)
+                        .fontWeight(.thin)
+                        .multilineTextAlignment(.center)
+                  
+                    Spacer()
+                    Text("8.8")
+                    Text("⭐️")
+                    
+                }
+            }
+            VStack{
+                Spacer()
+                HStack{
+                    Image("uyt")
+                        .resizable()
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        .frame(width: 130, height: 130)
+                        .scaledToFit()
+             
+                    Text("Kim's counvenence")
+                        .font(.body)
+                        .fontWeight(.thin)
+                        .multilineTextAlignment(.center)
+                    Spacer()
+                    Text("8.2")
+                    Text("⭐️")
+                  
+         
+                      
+                }
+            }
+            
+            VStack{
+                Spacer()
+                HStack{
+                    Image("key")
+                        .resizable()
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        .frame(width: 130, height: 130)
+                        .scaledToFit()
+               
+                    Text("lock and key")
+                        .font(.callout)
+                        .fontWeight(.thin)
+                        .multilineTextAlignment(.center)
+                    Spacer()
+                    Text("6.2")
+                    Text("⭐️")
+                    
+                    
+                  
+                }
+            }
+            
+            
         }
         
-            
-        ZStack{
-            VStack{
-            Image("uri")
-                .resizable()
-                .frame(width: 130, height: 130)
-                .scaledToFit()
-            }
-                
-        }
-            
-        ZStack{
-            VStack{
-            Image("image5")
-                .resizable()
-                .frame(width: 130, height: 130)
-                .scaledToFit()
-            }
-        }
-            ZStack{
-                VStack{
-            Image("uyt")
-                .resizable()
-                .frame(width: 130, height: 130)
-                .scaledToFit()
-                }
-            }
-            
-            ZStack{
-                VStack{
-            Image("key")
-                .resizable()
-                .frame(width: 130, height: 130)
-                .scaledToFit()
-                }
-            }
-            
-            
-        }
-   
     }
 }
 
